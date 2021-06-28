aea543ad35851d692c5197d19faf80f7c33f5305
Jorge Garcia <jorgegarc6@gmail.com>
2021-06-28 12:26:54 -0600

    Primer BE listo

---
+++ controllers/usuarios.js
@@ -0,0 +1,57 @@
+const { response, request } = require('express');
+
+const usuariosGet = (req = request, res = response) => {
+    
+    const { q, nombre, apikey, page = "1", limit} = req.query;
+
+    res.status(201).json({
+        msg: 'get API - controlador',
+        q,
+        nombre,
+        apikey,
+        page,
+        limit
+    });
+    
+}
+
+const usuariosPut = (req, res = response) => {
+    
+    const id = req.params.id;
+    
+    res.status(400).json({
+        msg: 'put API - controlador',
+        id
+    });
+}
+
+const usuariosPost = (req, res = response) => {
+    
+    const {nombre, edad} = req.body;
+
+    res.json({
+        msg: 'post API - controlador',
+        nombre,
+        edad
+    });
+}
+
+const usuariosDelete = (req, res = response) => {
+    res.json({
+        msg: 'delete API - controlador'
+    });
+}
+
+const usuariosPatch = (req, res = response) => {
+    res.json({
+        msg: 'patch API - controlador'
+    });
+}
+
+module.exports = {
+    usuariosGet,
+    usuariosPut,
+    usuariosPost,
+    usuariosDelete,
+    usuariosPatch
+}
