51359594ae60cb465ef9641fd2ee7ad84e101d7a
Jorge Garcia <jorgegarc6@gmail.com>
2021-07-14 09:36:58 -0600

    Google SignIn Added

---
+++ helpers/google-verify.js
@@ -0,0 +1,29 @@
+const { OAuth2Client } = require('google-auth-library');
+
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+
+const googleVerify = async( id_token='' ) => {
+  const ticket = await client.verifyIdToken({
+      idToken: id_token,
+      audience: process.env.GOOGLE_CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+      // Or, if multiple clients access the backend:
+      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+  });
+  
+  //Renombrar variables que vienen desde el payload, ex: name to "nombre" es nuevo alias
+  const { 
+      name: nombre, 
+      picture: img, 
+      email: correo } = ticket.getPayload();
+  //const userid = payload['sub'];
+  
+  return {
+      nombre,
+      img,
+      correo
+  };
+}
+
+module.exports = {
+    googleVerify
+}
